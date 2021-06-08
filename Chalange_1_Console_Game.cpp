aa89a63c396a2ebd5477961cc3be9976df61b2f4
Asku_SU <zaharandrei15@mail.ru>
2021-06-08 11:23:03 +0300

    Сделал движение дороги, и сделал вывод только при необходимости.

--- Chalange_1_Console_Game.cpp
+++ Chalange_1_Console_Game.cpp
@@ -22,7 +22,7 @@ typedef struct car
 		,{0x0020, 0x0020, 0x2588, 0x2588, 0x0020, 0x0020}
 		,{0x2588, 0x2588, 0x0020, 0x0020, 0x2588, 0x2588}
 	};
-	const short speedLevel[2] = { 0, 1 };
+	const float speedLevel[6] = { 0.f, 1.f, 1.5f, 2.f, 2.5f, 10.f };
 	const short acceleration = 2;
 } Car;
 
@@ -49,12 +49,45 @@ typedef struct track
 		0x0020, 0x0020, 0x0020, 0x0020, 0x0020
 	};
 	wchar_t trackField[heightTrack][widthTrack] = { {' '} };
+	short partLineNumber[heightTrack];
+	boolean NeedRefreshScreen = false;
 } Track;
 
 
 int nScreenWidth = 120;	
 int nScreenHeight = 40;
 
+
+void addNextLine(boolean isFirst, Track &currentTrack)
+{
+	currentTrack.NeedRefreshScreen = true;
+	for (short i = 0; i < currentTrack.heightTrack; i++)
+	{
+		if (!isFirst && i < currentTrack.heightTrack - 1)
+		{
+			wmemcpy(currentTrack.trackField[i], currentTrack.trackField[i + 1], currentTrack.widthTrack);
+			currentTrack.partLineNumber[i] = currentTrack.partLineNumber[i + 1];
+		}
+		else if (i != 0)
+		{
+			currentTrack.partLineNumber[i] = currentTrack.partLineNumber[i - 1] + 1;
+			if (currentTrack.partLineNumber[i] > 1 && currentTrack.partLineNumber[i] < 6)
+			{
+				wmemcpy(currentTrack.trackField[i], currentTrack.middlePart, currentTrack.widthTrack);
+			}
+			else
+			{
+				wmemcpy(currentTrack.trackField[i], currentTrack.emptyPart, currentTrack.widthTrack);
+				if (currentTrack.partLineNumber[i] > 2) currentTrack.partLineNumber[i] = 0;
+			}			
+		}
+		else
+		{
+			wmemcpy(currentTrack.trackField[i], currentTrack.startLine, currentTrack.widthTrack);
+			currentTrack.partLineNumber[i] = 5;
+		}
+	}
+}
 
 int main()
 {
@@ -69,7 +102,7 @@ int main()
 
 	Car newCar;
 	Track newTrack;
-	
+	/*
 	for (short i = 0; i < newTrack.heightTrack; i++)
 	{
 		if (i != 0)
@@ -89,23 +122,43 @@ int main()
 		else wmemcpy(newTrack.trackField[i], newTrack.startLine, newTrack.widthTrack);
 				
 	}
-
+	*/
+	addNextLine(true, newTrack);
 	auto tp1 = chrono::system_clock::now();
 	auto tp2 = chrono::system_clock::now();
 
-	while (1) {
-		for (short i = 0; i < newTrack.heightTrack; i++)
-		{
-			tp2 = chrono::system_clock::now();
-			chrono::duration<float> elapsedTime = tp2 - tp1;
-			tp1 = tp2;
-			float fElapsedTime = elapsedTime.count();
+	float V, TimeFromStart = 0;
+	long double S = 0;
+	int S_total = 0;
+	V = newCar.speedLevel[5];
 
-			//swprintf_s(newTrack.trackField[newTrack.heightTrack-1], newTrack.widthTrack, L"FPS=%3.0f",  1.0f / fElapsedTime);
-			
-			short invert = newTrack.heightTrack - i - 1;
-			WriteConsoleOutputCharacter(hConsole, newTrack.trackField[i], newTrack.widthTrack, { 0, invert }, &dwBytesWritten);
+	while (1) {
+		tp2 = chrono::system_clock::now();
+		chrono::duration<float> elapsedTime = tp2 - tp1;
+		tp1 = tp2;
+		float fElapsedTime = elapsedTime.count();
+
+		TimeFromStart += fElapsedTime;
+		if (!(TimeFromStart < 2))		
+		{
+			S += V * fElapsedTime;
+			if (S > S_total)
+			{
+				S_total++;
+				addNextLine(false, newTrack);
+			}
+		}		
+		if (newTrack.NeedRefreshScreen)
+		{
+			for (short i = 0; i < newTrack.heightTrack; i++)
+			{
+				//swprintf_s(newTrack.trackField[newTrack.heightTrack - 1], newTrack.widthTrack - 4, L"FPS=%4.3f", TimeFromStart);//1.0f / fElapsedTime);
+				short invert = newTrack.heightTrack - i - 1;
+				WriteConsoleOutputCharacter(hConsole, newTrack.trackField[i], newTrack.widthTrack, { 0, invert }, &dwBytesWritten);
+			}
+			newTrack.NeedRefreshScreen = false;
 		}
+		
 		//WriteConsoleOutputCharacter(hConsole, newCar.mas[0], 3, { 0,0 }, &dwBytesWritten);
 		//WriteConsoleOutputCharacter(hConsole, newCar.mas[1], 3, { 0,1 }, &dwBytesWritten);
 		//WriteConsoleOutputCharacter(hConsole, newTrack.middlePart, 12, { 0,2 }, &dwBytesWritten);
